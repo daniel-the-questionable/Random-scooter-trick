354b5d389abbb8d91546173976e58876852a3756
daniel-the-questionable <103897220+daniel-the-questionable@users.noreply.github.com>
2022-04-20 17:49:41 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,20 +2,21 @@ let x, z, trick, grind, amount, input, button1, button2, fullTrick;
 let a = 20;
 let b = 40;
 let tricks = ['Tailwhip', 'Heelwhip', 'Bar', 'Oppo bar'];
-let grinds = ['feeble', 'smith', '5050', '5-0', 'board slide', 'lip slide']
+let grinds = ['Feeble', 'Smith', '5050', '5-0', 'Board slide', 'Lip slide']
 let combo = [];
-let comboIndex = ['Trick', 'Grind'];
-let easyTricks = [];
-let mediumTricks = [];
-let hardTricks = [];
+let comboIndex = ['Easy Trick', 'Medium Trick', 'Hard Trick', 'Grind'];
+let easyTricks = ['Tailwhip', 'Heelwhip', 'Bar', 'Oppo bar', 'FS 180', 'BS 180'];
+let mediumTricks = ['Bartwist', 'Fingerwhip', 'FS 360', 'BS 360'];
+let hardTricks = ['Briflip', 'Inward', 'FS 540', 'BS 540'];
 let ledgeGrinds = [];
 let railGrinds = [];
 
 
 function setup() {
     removeElements();
-    createCanvas(375, 400);
+    createCanvas(400, 400);
     background(136,216,192);
+    combo = [];
     greeting = createElement('h2', 'Do you want a ledge');
     greeting.position(20, 5);
     greeting = createElement('h2', 'or rail trick?');
@@ -23,41 +24,90 @@ function setup() {
     button1 = createButton('Ledge');
     button1.position(20, 85);
     button1.mousePressed(ledge);
-  
     button2 = createButton('Rail');
     button2.position(80, 85);
     button2.mousePressed(rail);
   
-    button3 = createButton('Rail');
-    button3.position(80, 85);
-    button3.mousePressed(rail);
+    button11 = createButton('Edit trick list');
+    button11.position(20, 110);
+    button11.mousePressed(trickList);
 }
 
-Make button in setup for choosing trickList
 
+function draw(){
+    if (x==1){
+        removeElements();
+        x=3;
+        comboAmount();
+    }
+    else if (x==2){
+        removeElements();
+        x=4;
+        comboAmount();
+    }
+}
 
-function trickList (){
+function ledge(){
+    x = 1;
+} 
+function rail(){
+    x = 2;  
+}  
+
+function trickList(){
     removeElements();
+    greeting = createElement('h2', 'Edit trick lists here');
+    greeting.position(20, 5);
     
-    Make title
-
-    Make box to put text for easy, medium, hard, ledge grinds rail grinds
-    Make confirmation button “add”
-    Make it so the text entered will append 
-    createElement();
-    
-    createButton(‘h2’, “finished”);
-    buttonPush(setup);
+    greeting = createElement('a2', 'Add to easy trick list');
+    greeting.position(20, 65);
+    easyTrickInput = createInput();
+    easyTrickInput.position(20, 85);
+    easyTrickInput.size(65);
+    easyTrickInput.input();
+    button11 = createButton('Add to list');
+    button11.position(60, 85);
+    button11.mousePressed(editTrickList);
+  
+    greeting = createElement('h2', 'Add to medium trick list');
+    greeting.position(20, 5);
+    mediumTrickInput = createInput();
+    mediumTrickInput.position(20, 110);
+    mediumTrickInput.size(65);
+    button12 = createButton('Add to list');
+    button12.position(60, 110);
+    button12.mousePressed(editTrickList);
+  
+    greeting = createElement('h2', 'Add to hard trick list');
+    greeting.position(20, 5);
+    hardTrickInput = createInput();
+    hardTrickInput.position(20, 135);
+    hardTrickInput.size(65);
+    hardTrickInput.input();
+    button13 = createButton('Add to list');
+    button13.position(60, 135);
+    button13.mousePressed(editTrickList);
+  
+    greeting = createElement('h2', 'Add to grind list');
+    greeting.position(20, 5);
+    grindInput = createInput();
+    grindInput.position(20, 160);
+    grindInput.size(65);
+    grindInput.input();
+    button14 = createButton('Add to list');
+    button14.position(60, 160);
+    button14.mousePressed(editTrickList);
+  
+  
+  
 }
 
 
-Have i get + 1 if they add a trick and -1 if the remove a trick
-Need a way to remove words from the array(trick list)
-Start blank then have default trick lists button?
+function editTrickList(){
 
 
 
-
+}
 
 function comboAmount(){
     greeting = createElement('h2', 'How many tricks do you');
@@ -78,9 +128,9 @@ function comboAmount(){
     sel1.changed(mySelectEvent);
   
   
-    button4 = createButton('confirm');
-    button4.position(60, 85);
-    button4.mousePressed(pageSelector)
+    button3 = createButton('Confirm');
+    button3.position(60, 85);
+    button3.mousePressed(pageSelector);
 }
 function pageSelector(){
     if (amount == 1){
@@ -108,8 +158,14 @@ function mySelectEvent() {
 }
 
 function selectComboType1(){
-    if (sel2.value() == 'Trick'){
-        append(combo, 'Trick');
+    if (sel2.value() == 'Easy Trick'){
+        append(combo, 'Easy Trick');
+    }
+    if (sel2.value() == 'Medium Trick'){
+        append(combo, 'Medium Trick');
+    }
+    if (sel2.value() == 'Hard Trick'){
+        append(combo, 'Hard Trick');
     }
     if (sel2.value() == 'Grind'){
         append(combo, 'Grind');
@@ -117,40 +173,71 @@ function selectComboType1(){
     
 }
 function selectComboType2(){
-    if (sel3.value() == 'Trick'){
-        append(combo, 'Trick');
+
+    if (sel3.value() == 'Easy Trick'){
+        append(combo, 'Easy Trick');
+    }
+    if (sel3.value() == 'Medium Trick'){
+        append(combo, 'Medium Trick');
+    }
+    if (sel3.value() == 'Hard Trick'){
+        append(combo, 'Hard Trick');
     }
     if (sel3.value() == 'Grind'){
         append(combo, 'Grind');
     }
 }
 function selectComboType3(){
-    if (sel4.value() == 'Trick'){
-        append(combo, 'Trick');
+    if (sel4.value() == 'Easy Trick'){
+        append(combo, 'Easy Trick');
+    }
+    if (sel4.value() == 'Medium Trick'){
+        append(combo, 'Medium Trick');
+    }
+    if (sel4.value() == 'Hard Trick'){
+        append(combo, 'Hard Trick');
     }
     if (sel4.value() == 'Grind'){
         append(combo, 'Grind');
     }
 }
 function selectComboType4(){
-    if (sel5.value() == 'Trick'){
-        append(combo, 'Trick');
+    if (sel5.value() == 'Easy Trick'){
+        append(combo, 'Easy Trick');
+    }
+    if (sel5.value() == 'Medium Trick'){
+        append(combo, 'Medium Trick');
+    }
+    if (sel5.value() == 'Hard Trick'){
+        append(combo, 'Hard Trick');
     }
     if (sel5.value() == 'Grind'){
         append(combo, 'Grind');
     }
 }
 function selectComboType5(){
-    if (sel6.value() == 'Trick'){
-        append(combo, 'Trick');
+    if (sel6.value() == 'Easy Trick'){
+        append(combo, 'Easy Trick');
+    }
+    if (sel6.value() == 'Medium Trick'){
+        append(combo, 'Medium Trick');
+    }
+    if (sel6.value() == 'Hard Trick'){
+        append(combo, 'Hard Trick');
     }
     if (sel6.value() == 'Grind'){
         append(combo, 'Grind');
     }
 }
 function selectComboType6(){
-    if (sel7.value() == 'Trick'){
-        append(combo, 'Trick');
+    if (sel7.value() == 'Easy Trick'){
+        append(combo, 'Easy Trick');
+    }
+    if (sel7.value() == 'Medium Trick'){
+        append(combo, 'Medium Trick');
+    }
+    if (sel7.value() == 'Hard Trick'){
+        append(combo, 'Hard Trick');
     }
     if (sel7.value() == 'Grind'){
         append(combo, 'Grind');
@@ -169,13 +256,15 @@ function page31(){
     sel2 = createSelect();
     sel2.position(20, 85);
     sel2.option('Type');
-    sel2.option('Trick');
+    sel2.option('Easy Trick');
+    sel2.option('Medium Trick');
+    sel2.option('Hard Trick');
     sel2.option('Grind');
     sel2.selected('Type');
     sel2.changed(selectComboType1);
-    button5 = createButton('confirm');
-    button5.position(20, 110);
-    button5.mousePressed(page4);
+    button4 = createButton('confirm');
+    button4.position(20, 110);
+    button4.mousePressed(page4);
 }
 
 function page32(){
@@ -187,20 +276,24 @@ function page32(){
     sel2 = createSelect();
     sel2.position(20, 85);
     sel2.option('Type');
-    sel2.option('Trick');
+    sel2.option('Easy Trick');
+    sel2.option('Medium Trick');
+    sel2.option('Hard Trick');
     sel2.option('Grind');
     sel2.selected('Type');
     sel2.changed(selectComboType1);
     sel3 = createSelect();
     sel3.position(20, 110);
     sel3.option('Type');
-    sel3.option('Trick');
+    sel3.option('Easy Trick');
+    sel3.option('Medium Trick');
+    sel3.option('Hard Trick');
     sel3.option('Grind');
     sel3.selected('Type');
     sel3.changed(selectComboType2);
-    button5 = createButton('confirm');
-    button5.position(20, 135);
-    button5.mousePressed(page4);
+    button4 = createButton('confirm');
+    button4.position(20, 135);
+    button4.mousePressed(page4);
 }
 function page33(){
     removeElements();
@@ -211,27 +304,33 @@ function page33(){
     sel2 = createSelect();
     sel2.position(20, 85);
     sel2.option('Type');
-    sel2.option('Trick');
+    sel2.option('Easy Trick');
+    sel2.option('Medium Trick');
+    sel2.option('Hard Trick');
     sel2.option('Grind');
     sel2.selected('Type');
     sel2.changed(selectComboType1);
     sel3 = createSelect();
     sel3.position(20, 110);
     sel3.option('Type');
-    sel3.option('Trick');
+    sel3.option('Easy Trick');
+    sel3.option('Medium Trick');
+    sel3.option('Hard Trick');
     sel3.option('Grind');
     sel3.selected('Type');
     sel3.changed(selectComboType2);
     sel4 = createSelect();
     sel4.position(20, 135);
     sel4.option('Type');
-    sel4.option('Trick');
+    sel4.option('Easy Trick');
+    sel4.option('Medium Trick');
+    sel4.option('Hard Trick');
     sel4.option('Grind');
     sel4.selected('Type');
     sel4.changed(selectComboType3);
-    button5 = createButton('confirm');
-    button5.position(20, 160);
-    button5.mousePressed(page4);
+    button4 = createButton('confirm');
+    button4.position(20, 160);
+    button4.mousePressed(page4);
 }
 function page34(){
     removeElements();
@@ -242,34 +341,42 @@ function page34(){
     sel2 = createSelect();
     sel2.position(20, 85);
     sel2.option('Type');
-    sel2.option('Trick');
+    sel2.option('Easy Trick');
+    sel2.option('Medium Trick');
+    sel2.option('Hard Trick');
     sel2.option('Grind');
     sel2.selected('Type');
     sel2.changed(selectComboType1);
     sel3 = createSelect();
     sel3.position(20, 110);
     sel3.option('Type');
-    sel3.option('Trick');
+    sel3.option('Easy Trick');
+    sel3.option('Medium Trick');
+    sel3.option('Hard Trick');
     sel3.option('Grind');
     sel3.selected('Type');
     sel3.changed(selectComboType2);
     sel4 = createSelect();
     sel4.position(20, 135);
     sel4.option('Type');
-    sel4.option('Trick');
+    sel4.option('Easy Trick');
+    sel4.option('Medium Trick');
+    sel4.option('Hard Trick');
     sel4.option('Grind');
     sel4.selected('Type');
     sel4.changed(selectComboType3);
     sel5 = createSelect();
     sel5.position(20, 160);
     sel5.option('Type');
-    sel5.option('Trick');
+    sel5.option('Easy Trick');
+    sel5.option('Medium Trick');
+    sel5.option('Hard Trick');
     sel5.option('Grind');
     sel5.selected('Type');
     sel5.changed(selectComboType4);
-    button5 = createButton('confirm');
-    button5.position(20, 185);
-    button5.mousePressed(page4);
+    button4 = createButton('confirm');
+    button4.position(20, 185);
+    button4.mousePressed(page4);
 }
 function page35(){
     removeElements();
@@ -280,41 +387,51 @@ function page35(){
     sel2 = createSelect();
     sel2.position(20, 85);
     sel2.option('Type');
-    sel2.option('Trick');
+    sel2.option('Easy Trick');
+    sel2.option('Medium Trick');
+    sel2.option('Hard Trick');
     sel2.option('Grind');
     sel2.selected('Type');
     sel2.changed(selectComboType1);
     sel3 = createSelect();
     sel3.position(20, 110);
     sel3.option('Type');
-    sel3.option('Trick');
+    sel3.option('Easy Trick');
+    sel3.option('Medium Trick');
+    sel3.option('Hard Trick');
     sel3.option('Grind');
     sel3.selected('Type');
     sel3.changed(selectComboType2);
     sel4 = createSelect();
     sel4.position(20, 135);
     sel4.option('Type');
-    sel4.option('Trick');
+    sel4.option('Easy Trick');
+    sel4.option('Medium Trick');
+    sel4.option('Hard Trick');
     sel4.option('Grind');
     sel4.selected('Type');
     sel4.changed(selectComboType3);
     sel5 = createSelect();
     sel5.position(20, 160);
     sel5.option('Type');
-    sel5.option('Trick');
+    sel5.option('Easy Trick');
+    sel5.option('Medium Trick');
+    sel5.option('Hard Trick');
     sel5.option('Grind');
     sel5.selected('Type');
     sel5.changed(selectComboType4);
     sel6 = createSelect();
     sel6.position(20, 185);
     sel6.option('Type');
-    sel6.option('Trick');
+    sel6.option('Easy Trick');
+    sel6.option('Medium Trick');
+    sel6.option('Hard Trick');
     sel6.option('Grind');
     sel6.selected('Type');
     sel6.changed(selectComboType5);
-    button5 = createButton('confirm');
-    button5.position(20, 210);
-    button5.mousePressed(page4);
+    button4 = createButton('confirm');
+    button4.position(20, 210);
+    button4.mousePressed(page4);
 }
 function page36(){
     removeElements();
@@ -325,48 +442,60 @@ function page36(){
     sel2 = createSelect();
     sel2.position(20, 85);
     sel2.option('Type');
-    sel2.option('Trick');
+    sel2.option('Easy Trick');
+    sel2.option('Medium Trick');
+    sel2.option('Hard Trick');
     sel2.option('Grind');
     sel2.selected('Type');
     sel2.changed(selectComboType1);
     sel3 = createSelect();
     sel3.position(20, 110);
     sel3.option('Type');
-    sel3.option('Trick');
+    sel3.option('Easy Trick');
+    sel3.option('Medium Trick');
+    sel3.option('Hard Trick');
     sel3.option('Grind');
     sel3.selected('Type');
     sel3.changed(selectComboType2);
     sel4 = createSelect();
     sel4.position(20, 135);
     sel4.option('Type');
-    sel4.option('Trick');
+    sel4.option('Easy Trick');
+    sel4.option('Medium Trick');
+    sel4.option('Hard Trick');
     sel4.option('Grind');
     sel4.selected('Type');
     sel4.changed(selectComboType3);
     sel5 = createSelect();
     sel5.position(20, 160);
     sel5.option('Type');
-    sel5.option('Trick');
+    sel5.option('Easy Trick');
+    sel5.option('Medium Trick');
+    sel5.option('Hard Trick');
     sel5.option('Grind');
     sel5.selected('Type');
     sel5.changed(selectComboType4);
     sel6 = createSelect();
     sel6.position(20, 185);
     sel6.option('Type');
-    sel6.option('Trick');
+    sel6.option('Easy Trick');
+    sel6.option('Medium Trick');
+    sel6.option('Hard Trick');
     sel6.option('Grind');
     sel6.selected('Type');
     sel6.changed(selectComboType5);
     sel7 = createSelect();
     sel7.position(20, 210);
     sel7.option('Type');
-    sel7.option('Trick');
+    sel7.option('Easy Trick');
+    sel7.option('Medium Trick');
+    sel7.option('Hard Trick');
     sel7.option('Grind');
     sel7.selected('Type');
     sel7.changed(selectComboType6);
-    button5 = createButton('confirm');
-    button5.position(20, 235);
-    button5.mousePressed(page4);
+    button4 = createButton('confirm');
+    button4.position(20, 235);
+    button4.mousePressed(page4);
 }
 
 function page4(){
@@ -377,53 +506,52 @@ function page4(){
   
     for (let j = 0; j < amount; j++) {
         if (combo[j] == comboIndex[0]){
-            randomTrick();
-            fullTrick = createElement('h3', trick);
+            randomEasyTrick();
+            fullTrick = createElement('h3', easyTrick);
             fullTrick.position(20,b);
             b+=20;
         }
         if (combo[j] == comboIndex[1]){
+            randomMediumTrick();
+            fullTrick = createElement('h3', mediumTrick);
+            fullTrick.position(20,b);
+            b+=20;
+        }
+        if (combo[j] == comboIndex[2]){
+            randomHardTrick();
+            fullTrick = createElement('h3', hardTrick);
+            fullTrick.position(20,b);
+            b+=20;
+        }
+        if (combo[j] == comboIndex[3]){
             randomGrind();
             fullTrick = createElement('h3', grind);
             fullTrick.position(20,b);
             b+=20;
         }
         if (j==amount-1){
-            button6 = createButton('Randomize');
-            button6.position(20, b + 25);
-            button6.mousePressed(page4);
-            button7 = createButton('Back to start');
-            button7.position(20, b + 50);
-            button7.mousePressed(setup);
+            button5 = createButton('Randomize');
+            button5.position(20, b + 25);
+            button5.mousePressed(page4);
+            button6 = createButton('Back to start');
+            button6.position(20, b + 50);
+            button6.mousePressed(setup);
         }
     }
 }
 
-function randomTrick(){
-    trick = random(tricks);
-    
+function randomEasyTrick(){
+    easyTrick = random(easyTricks);
+}
+
+function randomMediumTrick(){
+    mediumTrick = random(mediumTricks);
+}
+
+function randomHardTrick(){
+    hardTrick = random(hardTricks);
 }
 
 function randomGrind(){
     grind = random(grinds);
 }
-
-function draw(){
-    if (x==1){
-        removeElements();
-        x=3;
-        comboAmount();
-    }
-    else if (x==2){
-        removeElements();
-        x=4;
-        comboAmount();
-    }
-}
-
-function ledge(){
-    x = 1;
-} 
-function rail(){
-    x = 2;  
-} 
